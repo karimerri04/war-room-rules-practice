97bdca4310d8b50414d76d466e3b1b8d2f23787e
karimerri04 <karimerri04@gmail.com>
2026-06-01 15:24:13 -0400

    chore: initialize project structure with Spring Boot backend

---
+++ backend/rulespractice/src/main/java/com/karimerri/rulespractice/RulespracticeApplication.java
@@ -0,0 +1,13 @@
+package com.karimerri.rulespractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulespracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulespracticeApplication.class, args);
+	}
+
+}
---
+++ backend/rulespractice/src/test/java/com/karimerri/rulespractice/RulespracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karimerri.rulespractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulespracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
