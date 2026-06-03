cdb57e4adb668d5e6ea7f0ede3f7ca92dc27efba
karimerri04 <karimerri04@gmail.com>
2026-06-03 11:01:06 -0400

    feat: initialize java rules service

---
+++ backend/java-rules-service/src/main/java/com/karimerri/warroom/javarules/JavaRulesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.karimerri.warroom.javarules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRulesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRulesServiceApplication.class, args);
+	}
+
+}
---
+++ backend/java-rules-service/src/test/java/com/karimerri/warroom/javarules/JavaRulesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karimerri.warroom.javarules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRulesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
